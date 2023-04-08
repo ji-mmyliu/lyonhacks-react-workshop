e77ccf07cfb724b98b3121aff3a3a7ae54011c65
Jimmy Liu <jimmyjcl753@gmail.com>
2023-04-08 12:23:59 -0400

    Initial commit

---
+++ components/tasks.jsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import { Snackbar, Alert } from "@mui/material";
+
+export function Tasks() {
+    // For tasks
+    const [tasks, setTasks] = React.useState([]); // List of all tasks
+    const [taskContent, setTaskContent] = React.useState(""); // New task input field content
+
+    // For messages
+    const [message, setMessage] = React.useState("");
+    const [alertType, setAlertType] = React.useState("");
+
+    function alert(message, type) {
+        setMessage(message);
+        setAlertType(type);
+    }
+
+    return (
+        <>
+            <link rel="stylesheet" href="static/style.css" />
+            <div className="container">
+                <div id="newtask">
+                    <form onSubmit={(ev) => {
+                        ev.preventDefault(); // Ensure the whole window doesn't reload because of the button
+                        if (taskContent) {
+                            tasks.push(taskContent); // Add the new task into the list of tasks
+                            setTasks([...tasks]); // Trigger re-render of the screen with new tasks list
+                            setTaskContent(""); // Reset the input field state
+
+                            alert("Successfully added a new task!", "success");
+                        }
+                        else {
+                            alert("Please enter some content for the task", "error");
+                        }
+                    }}>
+                        <input placeholder="Task to be done.." value={taskContent} onChange={(ev) => {
+                            setTaskContent(ev.target.value);
+                        }} />
+                        <button id="push">Add</button>
+                    </form>
+                </div>
+                <div id="tasks">
+                    {
+                        tasks.length > 0 ?
+                            tasks.map((task, idx) => {
+                                return (
+                                    <div className="task" key={idx}>
+                                        <span>{task}</span>
+                                        <form onSubmit={(ev) => {
+                                            ev.preventDefault();
+                                            tasks.splice(idx, 1);
+                                            setTasks([...tasks]);
+                                        }}>
+                                            <button type="submit">Delete</button>
+                                        </form>
+                                    </div>
+                                );
+                            })
+                            :
+                            <div className="task">
+                                <span><em>No tasks yet. Add one above!</em></span>
+                            </div>
+                    }
+                </div>
+
+                <span id="footer">Signed in as (username). <a href="/logout">Sign out</a></span>
+            </div>
+
+            <Snackbar open={!!message} autoHideDuration={3000} onClose={() => {}}>
+                <Alert onClose={() => {}} severity={alertType} sx={{ width: '100%' }}>
+                    {message}
+                </Alert>
+            </Snackbar>
+        </>
+    );
+}
---
+++ index.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Link, Route, Routes, useNavigate } from "react-router-dom";
+import { Tasks } from "./components/tasks";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/tasks" element={<Tasks />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
